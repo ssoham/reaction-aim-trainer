a10f64a84a72ab7bf2b8df787cae6530cd1fa9e1
rohit kundu <25834070+rkunds@users.noreply.github.com>
2021-11-18 12:49:30 -0600

    feat: basic aim trainer functionality, need to implement mouse button click event

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 use quicksilver::{
-    geom::Vector,
+    geom::{Circle, Vector},
     graphics::{Color, VectorFont},
     input::{Event, Key},
     run, Graphics, Input, Result, Settings, Window,
 };
+
+use std:: {io, thread, time};
 use rand::Rng;
-use std::io;
-use std::thread::sleep;
-use std::time::Duration;
-use std::{thread, time};
 
 
 fn main() {
@@ -29,15 +27,27 @@ fn main() {
                 title: "Reaction Timer",
                 ..Settings::default()
             },
-            app,
+            reaction_time,
         );
     }
     else if input == "a" {
-        // TODO: load a basic white gui, randomly placing and removing target images 
+        println!("You have chosen to test your aim!");
+        run(
+            Settings {
+                size: Vector { x: 800.0, y: 600.0 },
+                title: "Aim trainer",
+                ..Settings::default()
+            },
+            aim_trainer,
+        );
+    }
+    else {
+        println!("You have entered an invalid input. Please try again.");
+        main();
     }
 }
 
-async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {
+async fn reaction_time(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {
 
     gfx.clear(Color::WHITE);
     gfx.present(&window)?;
@@ -79,5 +89,43 @@ async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> 
     }
     
 
+    Ok(())
+}
+
+async fn aim_trainer(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {
+    let mut rand_pos = Vector::new(0.0, 0.0);
+    let mut target_exists = false;
+    let mut count = 0;
+    let mut average_time = 0.0;
+    let mut start_time = time::SystemTime::now();
+    let mut end_time = time::SystemTime::now();
+    loop {
+        while let Some(_) = input.next_event().await {}
+        gfx.clear(Color::WHITE);
+        if !target_exists {
+            rand_pos = Vector::new(rand::thread_rng().gen_range(50..750) as f32, rand::thread_rng().gen_range(50..550) as f32);
+            gfx.fill_circle(&Circle::new(rand_pos, 20.0), Color::RED);
+            target_exists = true;
+            count = count + 1;
+            start_time = time::SystemTime::now();
+        } else {
+            gfx.fill_circle(&Circle::new(rand_pos, 20.0), Color::RED);
+        }
+        let mouse = gfx.screen_to_camera(&window, input.mouse().location());
+        if mouse.distance(rand_pos) < 10.0 {
+            target_exists = false;
+            gfx.fill_circle(&Circle::new(rand_pos, 20.0), Color::WHITE);
+            end_time = time::SystemTime::now();
+            println!("{}", end_time.duration_since(start_time).unwrap().as_millis());
+            average_time += end_time.duration_since(start_time).unwrap().as_millis() as f32;
+        }
+        gfx.fill_circle(&Circle::new(mouse, 20.0), Color::RED);
+        if count == 10 {
+            average_time = average_time / 10.0;
+            println!("Average reaction time: {} ms", average_time);
+            break;
+        }
+        gfx.present(&window)?;
+    }
     Ok(())
 }
